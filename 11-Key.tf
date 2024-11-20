371dd99de2386a504ee5cbffb39cb7d1a371ee0f
CloudMarauders <cloudmarauders@gmail.com>
2024-11-19 18:05:21 -0600

    11-Key.tf

--- 11-Key.tf
+++ 11-Key.tf
@@ -1,21 +1,21 @@
-resource "tls_private_key" "Basiclinux" {
+resource "tls_private_key" "XXXX" {
   algorithm = "RSA"
   rsa_bits = 2048
 }
 
-data "tls_public_key" "Basiclinux" {
-  private_key_pem = tls_private_key.Basiclinux.private_key_pem
+data "tls_public_key" "XXXX" {
+  private_key_pem = tls_private_key.XXXX.private_key_pem
   
 }
 
 
 output "private_key" {
-  value = tls_private_key.Basiclinux.private_key_pem
+  value = tls_private_key.XXXX.private_key_pem
   sensitive = true
 }
 
 output "public_key" {
-  value = data.tls_public_key.Basiclinux.public_key_openssh
+  value = data.tls_public_key.XXXX.public_key_openssh
   
 }
 
